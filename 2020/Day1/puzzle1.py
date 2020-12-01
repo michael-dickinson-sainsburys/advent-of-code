653a0ce2f6883097e0c3db6a05c66f7180965a55
Michael Dickinson <45626543+michael-dickinson-sainsburys@users.noreply.github.com>
2020-12-01 10:05:56 +0000

    style: Fix linting issues

--- 2020/Day1/puzzle1.py
+++ 2020/Day1/puzzle1.py
@@ -3,14 +3,14 @@ from itertools import product
 sum_total = 2020
 
 with open("input_file.txt") as input_file:
-  raw_input = input_file.read().splitlines()
+    raw_input = input_file.read().splitlines()
 
-input = [
-  int(x.strip())
-  for x in raw_input
+input_values = [
+    int(x.strip())
+    for x in raw_input
 ]
 
-for values in product(input, input):
-  if sum(list(values)) == sum_total:
-    print(values)
-    print(f"The product of the pair is: {values[0] * values[1]}")
+for values in product(input_values, input_values):
+    if sum(list(values)) == sum_total:
+        print(values)
+        print(f"The product of the pair is: {values[0] * values[1]}")
