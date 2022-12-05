8a779fbee5d421da00c8866a9d2e04486926e656
Michael Dickinson <45626543+michael-dickinson-sainsburys@users.noreply.github.com>
2022-12-05 16:33:11 +0000

    feat: solution for part two on day 3 2022

--- 2022/day3/index.js
+++ 2022/day3/index.js
@@ -16,9 +16,26 @@ const convertToNumber = (letter) => {
     return items.indexOf(letter) + 1
 }
 
-const inputFile = 'test.txt'
-// const inputFile = 'puzzle.txt'
+// const inputFile = 'test.txt'
+const inputFile = 'puzzle.txt'
 const data = fs.readFileSync(inputFile).toString()
 const wronglyPackedItems = data.split('\n').map(rucksack => findDuplicatedItems(splitContentsInHalf(rucksack))).flat()
 const scores = wronglyPackedItems.map(letter => convertToNumber(letter))
 console.log(scores.reduce((runningTotal, value) => runningTotal + value))
+
+const sortShortestFirst = (list) => {
+    return list.sort((a, b) => a.length - b.length)
+}
+
+const findCommonItemInRucksacks = (rucksacks) => {
+    const sortedRucksacks = sortShortestFirst(rucksacks)
+    return [...new Set(sortedRucksacks[0].split('').filter(letter => sortedRucksacks[1].includes(letter) && sortedRucksacks[2].includes(letter)))]
+}
+
+const rucksacks = data.split('\n')
+let badges = []
+for (let i = 0; i < rucksacks.length - 2; i = i + 3) {
+    badges.push(findCommonItemInRucksacks(rucksacks.slice(i, i + 3)))
+}
+const total = badges.flat().map(letter => convertToNumber(letter)).reduce((runningTotal, value) => runningTotal + value)
+console.log(total)
