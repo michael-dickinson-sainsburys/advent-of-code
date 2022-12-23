49728422b6ec9d8c42bc99b55943e959530a923e
Michael Dickinson <michael.dickinson@sainsburys.co.uk>
2022-12-23 11:20:48 +0000

    refactor: day 9 2022 for readability and reusability

--- 2022/day9/index.js
+++ 2022/day9/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-const moveHead = (direction, position) => {
+const move = (direction, position) => {
     if (direction === 'R') position[0] += 1
     if (direction === 'L') position[0] -= 1
     if (direction === 'U') position[1] += 1
@@ -8,41 +8,36 @@ const moveHead = (direction, position) => {
     return position
 }
 
+const tailAdjacentToHead = (headPosition, tailPosition) => {
+    return (Math.abs(headPosition[0] - tailPosition[0]) <= 1 && Math.abs(headPosition[1] - tailPosition[1]) <= 1)
+}
+
+const moveTailTowardsHead = (headPosition, tailPosition) => {
+    if ((headPosition[0] - tailPosition[0]) >= 1) tailPosition = move('R', tailPosition)
+    if ((tailPosition[0] - headPosition[0]) >= 1) tailPosition = move('L', tailPosition)
+    if ((headPosition[1] - tailPosition[1]) >= 1) tailPosition = move('U', tailPosition)
+    if ((tailPosition[1] - headPosition[1]) >= 1) tailPosition = move('D', tailPosition)
+    return tailPosition
+}
+
+
 const moveTail = (headPosition, tailPosition) => {
-    if (!(Math.abs(headPosition[0] - tailPosition[0]) <= 1 && Math.abs(headPosition[1] - tailPosition[1]) <= 1)) {
-        if ((headPosition[0] - tailPosition[0]) >= 1) tailPosition[0] += 1
-        else if ((tailPosition[0] - headPosition[0]) >= 1) tailPosition[0] -= 1
-        if ((headPosition[1] - tailPosition[1]) >= 1) tailPosition[1] += 1
-        else if ((tailPosition[1] - headPosition[1]) >= 1) tailPosition[1] -= 1
+    if (!tailAdjacentToHead(headPosition, tailPosition)) {
+        tailPosition = moveTailTowardsHead(headPosition, tailPosition)
     }
     return tailPosition
 }
-const partOne = (lines) => {
-    let vistedPositionsByTail = new Set()
-    let head = [0, 0]
-    let tail = [0, 0]
-    vistedPositionsByTail.add(tail.toString())
-    for (const line of lines) {
-        const [direction, distance] = line.split(' ')
-        for (let i = 0; i < distance; i++) {
-            head = moveHead(direction, head)
-            tail = moveTail(head, tail)
-            vistedPositionsByTail.add(tail.toString())
-        }
-    }
-    return vistedPositionsByTail.size
-}
 
-const partTwo = (lines) => {
+const trackKnots = (lines, knotsToTrack) => {
     let vistedPositionsByTail = new Set()
     let head = [0, 0]
-    let tails = new Array(9).fill(0).map(() => new Array(2).fill(0))
+    let tails = new Array(knotsToTrack).fill(0).map(() => new Array(2).fill(0))
     let previousKnot
     vistedPositionsByTail.add([0, 0].toString())
     for (const line of lines) {
         const [direction, distance] = line.split(' ')
         for (let i = 0; i < distance; i++) {
-            head = moveHead(direction, head)
+            head = move(direction, head)
             previousKnot = head
             for (let tail of tails) {
                 tail = moveTail(previousKnot, tail)
@@ -51,11 +46,14 @@ const partTwo = (lines) => {
             vistedPositionsByTail.add(tails.slice(-1).toString())
         }
     }
-    return vistedPositionsByTail.size
+    return vistedPositionsByTail
 }
 
 const inputFile = 'puzzle.txt'
 const lines = fs.readFileSync(inputFile).toString().split('\n')
 
-console.log(partOne(lines))
-console.log(partTwo(lines))
+const partOne = trackKnots(lines, 1)
+console.log(partOne.size)
+
+const partTwo = trackKnots(lines, 9)
+console.log(partTwo.size)
